52a450391bbe8b873d88cdbcae8b5ba22d514a11
Razan <razanalzannan@gmail.com>
2023-05-27 02:03:26 +0300

    Initial Commit

---
+++ JSONAssignment/JSONAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONAssignmentApp.swift
+//  JSONAssignment
+//
+//  Created by Razan Mohammed Alzannan on 07/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct JSONAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
